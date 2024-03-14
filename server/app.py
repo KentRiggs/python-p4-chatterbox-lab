e67debf3bb295b8889c7e2e925e70f714c7595bf
kent riggs <k2r3@proton.me>
2024-03-14 07:58:29 -0700

    added code to satisfy tests

--- server/app.py
+++ server/app.py
@@ -14,13 +14,44 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = []
+        messages = [message.to_dict() for message in Message.query.all()]
+        response = make_response(messages, 200)
+        return response
+    
+    elif request.method == 'POST':      
+       data = request.get_json()
+       new_message = Message(body=data['body'], username=data['username'])
+       db.session.add(new_message)
+       db.session.commit()
 
-@app.route('/messages/<int:id>')
+       return jsonify(new_message.to_dict()), 201
+    
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    
+    message = Message.query.filter(Message.id == id).first()
 
+    if request.method == 'PATCH':
+        for attr in request.json:
+            setattr(message, attr, request.json.get(attr))
+        db.session.commit()
+
+        return (jsonify(message.to_dict()), 200)
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Review deleted."
+        }
+
+        return (response_body, 200)
+    
 if __name__ == '__main__':
     app.run(port=5555)
